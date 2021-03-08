df97e27b5b9880ee605426aeda2083ca244d0608
StephanP <1051233700@qq.com>
2021-03-08 15:47:41 +0800

    前端完成了login登录功能，但是后端还没有实现

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css' // 导入全局样式
+import './assets/fonts/iconfont.css' // 导入字体图标
+import axios from 'axios'
+// TODO 这里可以替换为后端的API根路径
+// axios.defaults.baseURL = '' // 配置请求的根路径
+Vue.prototype.$http = axios // 把axios挂载到Vue原型对象上
 
 Vue.config.productionTip = false
 
