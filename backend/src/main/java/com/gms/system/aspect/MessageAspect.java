d74420f8e174a813636e820d486e20bbe8e52e6b
晋必成 <jinbicheng029@outlook.com>
2021-03-22 17:18:12 +0800

    自动通知进行中。教研处返回论文结果增添了学生信息

--- backend/src/main/java/com/gms/system/aspect/MessageAspect.java
+++ backend/src/main/java/com/gms/system/aspect/MessageAspect.java
@@ -7,6 +7,7 @@ import com.gms.system.utils.CreatNewMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -20,17 +21,17 @@ public class MessageAspect {
     /**
      * 答辩安排的相应通知切点
      */
-    @AfterReturning(value = "execution(* com.gms.gms.controller.PleaController.updatePleaReleaseTure(com.alibaba.fastjson.JSONObject)) && args(jsonObject)",argNames = "jsonObject")
+    @AfterReturning(value = "execution(* com.gms.gms.controller.PleaController.updatePleaReleaseTure(com.alibaba.fastjson.JSONObject)) && args(jsonObject)", argNames = "jsonObject")
     public void afterUpdatePleaReleaseTure(JSONObject jsonObject) {
         new CreatNewMessage().creatNewPleaOne(jsonObject.getString("stage"));
     }
 
-    @AfterReturning(value = "execution(* com.gms.gms.controller.PleaController.updatePleaReleaseFalse(com.alibaba.fastjson.JSONObject)) && args(jsonObject)",argNames = "jsonObject")
+    @AfterReturning(value = "execution(* com.gms.gms.controller.PleaController.updatePleaReleaseFalse(com.alibaba.fastjson.JSONObject)) && args(jsonObject)", argNames = "jsonObject")
     public void afterUpdatePleaReleaseFalse(JSONObject jsonObject) {
         new CreatNewMessage().creatNewPleaTwo(jsonObject.getString("stage"));
     }
 
-    @AfterReturning(value = "execution(* com.gms.gms.controller.PleaController.deletePleaRelease(java.lang.String)) && args(stage)",argNames = "stage")
+    @AfterReturning(value = "execution(* com.gms.gms.controller.PleaController.deletePleaRelease(java.lang.String)) && args(stage)", argNames = "stage")
     public void afterUpdatePleaReleaseFalse(String stage) {
         new CreatNewMessage().creatNewPleaTwo(stage);
     }
@@ -38,7 +39,7 @@ public class MessageAspect {
     /**
      * 学生选题状态更新通知切点
      */
-    @AfterReturning(value = "execution(* com.gms.gms.controller.AppliedSubjectController.auditAppliedSubject(com.gms.gms.domain.AppliedSubject)) && args(appliedSubject)",argNames = "appliedSubject")
+    @AfterReturning(value = "execution(* com.gms.gms.controller.AppliedSubjectController.auditAppliedSubject(com.gms.gms.domain.AppliedSubject)) && args(appliedSubject)", argNames = "appliedSubject")
     public void afterAuditAppliedSubject(AppliedSubject appliedSubject) {
         new CreatNewMessage().creatNewStudentAppliedSubject(appliedSubject.getStudentId());
     }
@@ -57,5 +58,29 @@ public class MessageAspect {
     /*@AfterReturning(value = "execution(* com.gms.gms.controller.ReportController.auditReport(com.gms.gms.domain.Report)) && args(report)",argNames = "report")
     public void afterAuditReport(Report report) {
         new CreatNewMessage().creatNewAuditReport(report.getId());
+    }*/
+
+    /**
+     * 论文安排发布后的自动通知
+     */
+    @AfterReturning(value = "execution(* com.gms.gms.controller.ThesisTeacherController.updateThesisGroupStage())")
+    public void afterUpdateThesisGroupStage() {
+        new CreatNewMessage().afterUpdateThesisGroupStage();
+    }
+
+    /**
+     * 论文安排删除前的自动通知
+     */
+    @Before(value = "execution(* com.gms.gms.controller.ThesisTeacherController.deleteThesisGroupStage())")
+    public void beforeDeleteThesisGroupStage() {
+        new CreatNewMessage().beforeDeleteThesisGroupStage();
+    }
+
+    /**
+     * 老师论文打分后的自动通知和结果统计
+     */
+    /*@AfterReturning(value = "execution(* com.gms.gms.controller.ThesisTeacherController.scoreThesisGroupStage())")
+    public void afterUpdateThesisGroupStage() {
+        new CreatNewMessage().afterUpdateThesisGroupStage();
     }*/
 }
